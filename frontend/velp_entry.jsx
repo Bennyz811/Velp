4e69a4f5b422a2ef1204329fa7e3567214b81db0
Benny Zhao <bennyz811@gmail.com>
2017-10-24 16:40:25 -0400

    got it on local host

--- frontend/velp_entry.jsx
+++ frontend/velp_entry.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Root from './components/root'
 import configureStore from './store/store.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const store = configureStore();
   window.getState = store.getState;
   window.dispatch = store.dispatch;
+  const store = configureStore();
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>kjhsgkjhajks</h1>, root);
+  ReactDOM.render(<Root store={store} />, root);
 })
