d91b95a7bd1cc79c173bf5ae75d39d80e1f489f9
Benny Zhao <bennyz811@gmail.com>
2018-02-01 12:32:41 -0500

    fixed nav bar styling

--- frontend/components/business/universal_nav.jsx
+++ frontend/components/business/universal_nav.jsx
@@ -2,16 +2,19 @@ import SearchContainer from './search_container';
 import GreetingContainer from '../greeting/greeting_container';
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {AuthRoute} from '../../util/route_util';
+import SessionFormContainer from '../session/session_form_container';
 
 class UniversalNav extends React.Component{
   render(){
-    return(
-      <div className="main-nav-wrapper">
+    return <div className="main-nav-wrapper">
         <div className="uni-search-container">
           <div className="search-bar-nav">
             <div className="uni-arr-container">
               <div className="uni-logo">
-                <Link className="nav-home-link" to='/'><img className='main-nav-logo' src={window.velpLogo}/></Link>
+                <Link className="nav-home-link" to="/">
+                  <img className="main-nav-logo" src={window.velpLogo} />
+                </Link>
               </div>
               <div className="nav-search-bar">
                 <SearchContainer />
@@ -19,8 +22,7 @@ class UniversalNav extends React.Component{
             </div>
           </div>
         </div>
-      </div>
-    );
+      </div>;
   }
 }
 
