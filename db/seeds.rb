89cec617f1fbc2f595527b1bc3c250037a02f7c3
Benny Zhao <bennyz811@gmail.com>
2018-03-03 20:25:56 -0500

    faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'faker'
 Business.destroy_all
 User.destroy_all
 Review.destroy_all
