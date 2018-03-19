11d7cf920257325c5ad0d282cf825bb7a0c41c20
Benny Zhao <bennyz811@gmail.com>
2018-03-19 17:12:02 -0400

    started infinite scroll

--- frontend/components/business/business_index.jsx
+++ frontend/components/business/business_index.jsx
@@ -4,10 +4,12 @@ import IndexItem from './business_index_item';
 import UniversalNav from './universal_nav';
 import BusinessMap from '../business_map/business_map';
 import Footer from '../footer.jsx';
+import InfiniteScroll from 'react-infinite-scroller';
 
 class BusinessIndex extends React.Component {
   constructor(props){
     super(props);
+    this.handleScroll = this.handleScroll.bind(this);
   }
 
   componentDidMount(){
@@ -15,41 +17,43 @@ class BusinessIndex extends React.Component {
     // this.props.updateFilter("category");
   }
 
-  render(){
-    return (
-      <div>
-        <UniversalNav />
-        <div className="result-super-container">
-          <div className="result-container">
+  handleScroll(){
+    if (this.scroller){
+      console.log(this.scroller.scrollTop);
+    }
+  }
 
+  render(){
+    return <div>
+        <UniversalNav />
+        <div onScroll={this.handleScroll} ref={scroller => {this.scroller = scroller;}} className="result-super-container">
+          <div className="result-container">
             <div className="result-col-alpha">
               <div className="result-list">
                 <ul id="index" className="result-list-ul">
-                    {
-                      this.props.businesses.map(business => (
-                        <IndexItem
-                          className="result-list-ind"
-                          key={business.id}
-                          businesses={business} />
-                      ))
-                    }
+                    {this.props.businesses.map(business => (
+                      <IndexItem
+                        className="result-list-ind"
+                        key={business.id}
+                        businesses={business}
+                      />
+                    ))}
                 </ul>
-                {!this.props.businesses.length && <p className="error-msg">Sorry! There are no results matching your search. Please try another term.</p>}
+                {!this.props.businesses.length && <p className="error-msg">
+                    Sorry! There are no results matching your search. Please
+                    try another term.
+                  </p>}
               </div>
             </div>
 
             <div className="result-col-beta">
               <div className="map-container">
-                  <BusinessMap
-                    businesses={this.props.businesses}
-                    />
+                <BusinessMap businesses={this.props.businesses} />
               </div>
             </div>
           </div>
-
-      </div>
-      </div>
-    );
+        </div>
+      </div>;
   }
 }
 export default BusinessIndex;
