3ab97c4a53e9e025f8b8d1d557856c134385d9f3
Benny Zhao <bennyz811@gmail.com>
2018-03-21 19:41:32 -0400

    fixed params

--- frontend/components/business/business_show.jsx
+++ frontend/components/business/business_show.jsx
@@ -14,7 +14,7 @@ class BusinessShow extends React.Component {
   }
 
   componentDidMount(){
-    this.props.fetchBusiness(this.props.bizId);
+    this.props.fetchBusiness(this.props.match.params.businessId);
   }
 
   // componentWillReceiveProps(nextProps){
