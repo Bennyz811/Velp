d0708d56d2c79ebc78013f28be156d7d88b918c2
Benny Zhao <bennyz811@gmail.com>
2018-03-27 14:40:58 -0400

    infite scroll

--- frontend/components/business/business_index.jsx
+++ frontend/components/business/business_index.jsx
@@ -11,34 +11,29 @@ class BusinessIndex extends React.Component {
   constructor(props){
     super(props);
     this.handleScroll = this.handleScroll.bind(this);
-    this.handleClick = this.handleClick.bind(this);
   }
   
   componentDidMount(){
-    // const params = queryString.parseUrl(this.props.query.searchTerm);
-    // console.log(params);
+    window.scrollTo(0,0);
     const query = (this.props.location.search).slice(1);
     this.props.searchForBusinesses({searchTerm: query});
-    // this.props.updateFilter(params);
-    // this.props.location.push({
-    //   pathname: this.props.location.pathname,
-    //   search: queryString.parseUrl(this.props.query.searchTerm).url
-    // });
     window.addEventListener('scroll', this.handleScroll);
   }
   
   componentWillUpdate(){
   }
-  
+
   handleScroll(){
-    if (this.scroller){
-      console.log(`value=${this.scroller.scrollTop}`);
+    const x = document.getElementById('index');
+    console.log(x.height, x.clientHeight, window.pageYOffset, x.offsetHeight);
+    if (window.pageYOffset > Math.ceil(x.offsetHeight * 0.85)){
+      this.props.searchForBusinesses({ searchTerm: this.props.location.search.slice(1) }, this.props.offSet);
+      if (window.pageYOffset > x.offsetHeight){
+        window.removeEventListener('scroll', this.handleScroll);
+      }
     }
   }
 
-  handleClick(){
-    this.props.searchForBusinesses({searchTerm: (this.props.location.search).slice(1)}, this.props.offSet);
-  }
 
   render(){
     return <div>
@@ -47,17 +42,14 @@ class BusinessIndex extends React.Component {
           <div className="result-container">
             <div className="result-col-alpha">
               <div className="result-list">
-              <button onClick={this.handleClick}>Button</button>
-                <ul onScroll={this.handleScroll} ref={scroller => {
-                    this.scroller = scroller;
-                  }} id="index" className="result-list-ul">
-                  {this.props.businesses.map(business => (
-                    <IndexItem
-                      className="result-list-ind"
-                      key={business.id}
-                      businesses={business}
-                    />
-                  ))}
+                <ul id="index" className="result-list-ul">
+                    {this.props.businesses.map(business => (
+                      <IndexItem
+                        className="result-list-ind"
+                        key={business.id}
+                        businesses={business}
+                      />
+                    ))}
                 </ul>
                 {!this.props.businesses.length && <p className="error-msg">
                     Sorry! There are no results matching your search. Please
