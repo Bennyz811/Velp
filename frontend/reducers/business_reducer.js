a68a3e142a970408f12b296f77dea4cff196d24a
Benny Zhao <bennyz811@gmail.com>
2018-04-07 16:06:21 -0400

    refactored business reducer

--- frontend/reducers/business_reducer.js
+++ frontend/reducers/business_reducer.js
@@ -4,21 +4,9 @@ import {RECEIVE_ALL_BUSINESSES, RECEIVE_BUSINESS, REMOVE_BUSINESS, SEARCH_BUSINE
 let _initialState = {};
 const businessesReducer = (state = _initialState, action) => {
   Object.freeze(state);
-  console.log('business reducer')
   switch (action.type) {
-    case RECEIVE_ALL_BUSINESSES:
-      return action.businesses;
     case RECEIVE_BUSINESS:
-    // return merge({}, state, {[action.business.business.id]: action.business.business})
       return action.business;
-    // case SEARCH_BUSINESSES:
-    //   return state.concat(action.input);
-    // case SEARCH_NEW_BUSINESSES:
-    //   return action.input;
-    case REMOVE_BUSINESS:
-      let newState = [].concat(state);
-      delete newState[action.businessId];
-      return newState;
     default:
       return state;
   }
