2eb8a11b28b7fca97632e0b56e266c92ab8230fb
Benny Zhao <bennyz811@gmail.com>
2018-04-07 18:05:30 -0400

    refactored reducer

--- frontend/reducers/search_reducer.js
+++ frontend/reducers/search_reducer.js
@@ -1,17 +1,13 @@
 import merge from 'lodash/merge';
 import {SEARCH_BUSINESSES, SEARCH_NEW_BUSINESSES} from '../actions/business_actions';
 
-
-// const _initialState = {input: []};
 const _initialState = [];
 
 const searchReducer = (state = _initialState, action) => {
   Object.freeze(state);
 
-  console.log('search reducer')
   switch (action.type) {
     case SEARCH_BUSINESSES:
-      // return merge({}, state, {input: action.input})
       return state.concat(action.input);
     case SEARCH_NEW_BUSINESSES:
       return action.input;
