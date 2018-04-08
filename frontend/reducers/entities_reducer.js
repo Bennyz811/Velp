6188d4ca149864439c6159382a1b819569b29bc7
Benny Zhao <bennyz811@gmail.com>
2018-04-07 20:45:24 -0400

    combined recommendation state to entities

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,8 +2,10 @@ import {combineReducers} from 'redux';
 import businessesReducer from './business_reducer';
 import reviewReducer from './review_reducer';
 import searchReducer from './search_reducer';
+import recommendationReducer from './recommend_reducer';
 
 export default combineReducers({
   selectedBusiness: businessesReducer,
   searchResults: searchReducer,
+  recommendations: recommendationReducer
 });
