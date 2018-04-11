ca9958f2bb58700abab7817c6bbabd236da9b133
Benny Zhao <bennyz811@gmail.com>
2018-04-10 20:13:39 -0400

    refactored review actions

--- frontend/actions/review_actions.js
+++ frontend/actions/review_actions.js
@@ -1,4 +1,4 @@
-export const RECEIVE_ALL_REVIEWS = 'RECEIVE_ALL_REVIEWS';
+export const RECEIVE_BUSINESS_REVIEWS = 'RECEIVE_ALL_REVIEWS';
 export const RECEIVE_REVIEW = 'RECEIVE_REVIEW';
 export const REMOVE_REVIEW = 'REMOVE_REVIEW';
 export const UPDATE_REVIEW = 'UPDATE_REVIEW';
@@ -6,34 +6,35 @@ export const UPDATE_REVIEW = 'UPDATE_REVIEW';
 
 import * as ReviewAPIUtil from '../util/review_api_util';
 
-const receiveAllReviews = reviews => ({
-  type: RECEIVE_ALL_REVIEWS, reviews
-})
+const receiveBusinessReviews = businessReviews => ({
+  type: RECEIVE_BUSINESS_REVIEWS, businessReviews
+});
 
 const receiveReview = review => ({
   type: RECEIVE_REVIEW, review
-})
+});
 
 const removeReview = reviewId => ({
   type: REMOVE_REVIEW, reviewId
-})
+});
 
-export const fetchAllReviews = () => dispatch => {
-  return ReviewAPIUtil.fetchAllReviews().then(reviews => dispatch(receiveAllReviews(reviews)))
-}
+export const fetchBusinessReviews = (revBizId, offSet) => dispatch => {
+  return ReviewAPIUtil.fetchBusinessReviews(revBizId, offSet)
+    .then(businessReviews => dispatch(receiveBusinessReviews(businessReviews)));
+};
 
 export const fetchReview = id => dispatch => {
-  return ReviewAPIUtil.fetchReview(id).then(review => dispatch(receiveReview(review)))
-}
+  return ReviewAPIUtil.fetchReview(id).then(review => dispatch(receiveReview(review)));
+};
 
 export const createReview = review => dispatch => {
-  return ReviewAPIUtil.createReview(review).then(review => dispatch(receiveReview(review)))
-}
+  return ReviewAPIUtil.createReview(review).then(review => dispatch(receiveReview(review)));
+};
 
 export const updateReview = review => dispatch => {
-  return ReviewAPIUtil.updateReview(review).then(review => dispatch(receiveReview(review)))
-}
+  return ReviewAPIUtil.updateReview(review).then(review => dispatch(receiveReview(review)));
+};
 
 export const deleteReview = reviewId => dispatch => {
-  return ReviewAPIUtil.deleteReview(reviewId).then(review => dispatch(removeReview(review)))
-}
+  return ReviewAPIUtil.deleteReview(reviewId).then(review => dispatch(removeReview(review)));
+};
