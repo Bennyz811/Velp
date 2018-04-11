d70bb345eab2c3611f39336d5501d2e2f583acc7
Benny Zhao <bennyz811@gmail.com>
2018-04-10 20:07:07 -0400

    fixed and rename business review api

--- frontend/util/review_api_util.js
+++ frontend/util/review_api_util.js
@@ -1,36 +1,37 @@
-export const fetchAllReviews = () => {
+export const fetchBusinessReviews = (revBizId, offSet) => {
   return $.ajax({
     method: 'get',
-    url: 'api/reviews'
-  })
-}
+    url: 'api/reviews',
+    data: {revBizId, offSet}
+  });
+};
 
 export const fetchReview = id => {
   return $.ajax({
     method: 'get',
     url: `api/reviews/${id}`
-  })
-}
+  });
+};
 
 export const createReview = review => {
   return $.ajax({
     method: 'post',
     url: `api/reviews`,
     data: {review}
-  })
-}
+  });
+};
 
 export const updateReview = (review) => {
   return $.ajax({
     method: 'patch',
     url: `api/reviews/${review.id}`,
     data: {review}
-  })
-}
+  });
+};
 
 export const deleteReview = reviewId => {
   return $.ajax({
     method: 'delete',
     url: `api/reviews/${reviewId}`
-  })
-}
+  });
+};
