d9e7077129bf2d1bee888374f7d4dfe8e95a3be9
Benny Zhao <bennyz811@gmail.com>
2018-04-18 22:06:03 -0400

    new picture attachment file schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180419010502) do
+ActiveRecord::Schema.define(version: 20180419020330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,10 @@ ActiveRecord::Schema.define(version: 20180419010502) do
     t.integer "biz_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "reviews", force: :cascade do |t|
