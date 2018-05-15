cdb8de2371af209019f22ee3df6e094348551e1e
Benny Zhao <bennyz811@gmail.com>
2018-05-14 21:31:16 -0400

    removed dead code

--- frontend/reducers/review_reducer.js
+++ frontend/reducers/review_reducer.js
@@ -16,17 +16,8 @@ const reviewReducer = (state = _initialState, action) => {
       return action.businessReviews;
     case RECEIVE_MORE_BUSINESS_REVIEWS:
       return state.concat(action.businessReviews);
-    // case RECEIVE_REVIEW:
-    //   return merge({}, state, {[action.review.id]: action.review});
-    // case REMOVE_REVIEW:
-    //   let newState = merge({}, state);
-    //   delete newState[action.reviewId];
-    //   return newState;
-    // case RECEIVE_BUSINESS:
-    //   return merge({}, state, action.business.reviews);
     default:
       return state;
-
   }
 
 };
